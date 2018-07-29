45c81f587a47b35708196e532adea0c779435c15
pgigoux <pgigoux@gemini.edu>
2018-07-29 14:59:30 -0400

    Added is_in() funtion

--- pvtoken.py
+++ pvtoken.py
@@ -35,9 +35,20 @@ class PvToken:
         Check whether a token has a certain id
         :param token_id: token id
         :type token_id: int
+        :return true if token id matches the specified id
         :rtype: bool
         """
         return True if self.id == token_id else False
+
+    def is_in(self, token_id_list):
+        """
+        Check whether a token id is in a list of possible id's
+        :param token_id_list: list of token id's
+        :type token_id_list: list
+        :return: true if the token id is in the list
+        :rtype: bool
+        """
+        return True if self.id in token_id_list else False
 
 
 if __name__ == '__main__':
