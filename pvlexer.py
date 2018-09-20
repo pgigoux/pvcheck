9f5d108fddcc06eb60896a1aa492a357a7196101
pgigoux <pgigoux@gemini.edu>
2018-09-20 14:33:29 -0300

    Rewrote next_token. Added TOKEN_EOF.

--- pvlexer.py
+++ pvlexer.py
@@ -3,8 +3,9 @@ from pvtoken import PvToken
 
 # Token definitions
 TOKEN_NONE = 0
-TOKEN_WHITESPACE = 1
-TOKEN_COMMENT = 2
+TOKEN_EOF = 1
+TOKEN_WHITESPACE = 2
+TOKEN_COMMENT = 3
 # patterns
 TOKEN_INTEGER = 10
 TOKEN_REAL = 11
@@ -32,7 +33,6 @@ TOKEN_ERROR = -1
 
 
 class PvLexer:
-
     # Lexer regular expressions. The order matters!
     # In general, regular expressions are ordered with the most complex ones first.
     lexer_patterns = [
@@ -69,6 +69,7 @@ class PvLexer:
         """
         self.last_line = ''
         self.line_number = 0
+        self.token_list = []
         self.compiled_patterns = []
         for pattern, token_id in self.lexer_patterns:
             self.compiled_patterns.append((re.compile(pattern), token_id))
@@ -82,6 +83,7 @@ class PvLexer:
         :return: list of Tokens
         :rtype: list
         """
+        # print '_get_token_list'
         m = None
         line_pos = 0
         line_length = len(line)
@@ -90,23 +92,25 @@ class PvLexer:
 
         # Traverse the line starting from the first character
         while line_pos < line_length:
+
+            # Assign a default value so the IDE doesn't complain
             t_id = TOKEN_NONE
 
             # Loop over all the possible patterns looking for a match.
             # Break the loop if one is found and proceed to the next step.
-            # White spaces are ignored at this point.
+            # Comments and whitespaces are ignored at this point.
             for t_pat, t_id in self.compiled_patterns:
                 # print str(line_pos) + ': [' + str(line[line_pos:]) + ']'
                 m = t_pat.match(line, line_pos)
                 if m:
                     # print '+ [' + m.group() + '] ' + str(t_id)
-                    if t_id != TOKEN_WHITESPACE:  # ignore white spaces
+                    if t_id != TOKEN_WHITESPACE and t_id != TOKEN_COMMENT:
                         token_list.append(PvToken(t_id, m.group(0)))
                     break
 
-            # Check whether a match is found and move the character counter
+            # If a match was found then move the character counter
             # to the next non-processed character in the line.
-            # Comments are stripped at this point by breaking the loop.
+            # The rest of the comment line is stripped at this point.
             if m:
                 if t_id != TOKEN_COMMENT:
                     line_pos += len(m.group())
@@ -116,6 +120,7 @@ class PvLexer:
                 token_list.append(PvToken(TOKEN_ERROR, m.group()))
                 break
 
+        # print '-', token_list
         return token_list
 
     # def get_line_number(self):
@@ -130,33 +135,65 @@ class PvLexer:
     def get_last_line(self):
         return self.line_number, self.last_line
 
+    # def next_token(self, f_in):
+    #     """
+    #     Return next token in the file.
+    #     This is the main routine that will be called by the parser.
+    #     :param f_in: input file
+    #     :type f_in: file
+    #     :return: next token
+    #     :rtype:
+    #     """
+    #     for line in f_in:
+    #
+    #         line = line.strip()
+    #         self.line_number += 1
+    #
+    #         # Ignore comment and blank lines
+    #         if re.search(r'^#', line) or len(line) == 0:
+    #             continue
+    #
+    #         self.last_line = line
+    #
+    #         for token in self._get_token_list(line):
+    #             yield token
+
     def next_token(self, f_in):
         """
         Return next token in the file.
         This is the main routine that will be called by the parser.
+        It was not implemented as an iterator because of the parser requirements.
         :param f_in: input file
         :type f_in: file
         :return: next token
         :rtype: PvToken
         """
-        for line in f_in:
+        if len(self.token_list) == 0:
+            # print 'empty'
+            try:
+                # Look for the next non comment and non white line in the line
+                while True:
+                    line = f_in.next().strip()
+                    if re.search(r'^#', line) is None and len(line) > 0:
+                        break
 
-            line = line.strip()
-            self.line_number += 1
+                self.token_list = self._get_token_list(line)
+                self.last_line = line
+                self.line_number += 1
 
-            # Ignore comment and blank lines
-            if re.search(r'^#', line) or len(line) == 0:
-                continue
+            except StopIteration:
+                self.token_list = [PvToken(TOKEN_EOF, '')]
 
-            self.last_line = line
-
-            for token in self._get_token_list(line):
-                yield token
+        return self.token_list.pop(0)
 
 
 if __name__ == '__main__':
+
     lex = PvLexer()
 
-    with open('example.pv') as f:
-        for t in lex.next_token(f):
+    with open('example1.pv') as f:
+        while True:
+            t = lex.next_token(f)
             print t
+            if t.id == TOKEN_EOF:
+                break
