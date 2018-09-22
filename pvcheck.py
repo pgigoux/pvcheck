41bfb93251bac3b09016189cfb96c35b69e2fa89
pgigoux <pgigoux@gemini.edu>
2018-09-22 13:43:10 -0300

    First commit of the main program

---
+++ pvcheck.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+import sys
+from argparse import ArgumentParser, SUPPRESS
+from pvparser import PvParser
+
+
+if __name__ == '__main__':
+    """
+    Entry point for the pvload file check program.
+    """
+
+    # Command line arguments
+    parser = ArgumentParser(epilog='')
+
+    parser.add_argument(action='store',
+                        nargs='*',
+                        dest='file_list',
+                        default=[],
+                        help='list of input files')
+
+    parser.add_argument('-d',
+                        action='store_true',
+                        dest='debug',
+                        default=False,
+                        help=SUPPRESS)
+
+    args = parser.parse_args(sys.argv[1:])
+
+    pv_parser = PvParser()
+    pv_parser.set_debug(args.debug)
+
+    for file_name in args.file_list:
+        pv_parser.pv_file(file_name)
